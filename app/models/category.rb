df52cef73a8499a33b3e89b00c62c3410f567264
eric07109 <eric07109@gmail.com>
2017-12-15 08:30:18 +0800

    removed the dependent destroy relationship between category and restaurant so that removing a category will not automatically remove all related restaurants

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
 	validates :name, presence: true, uniqueness: true
-	has_many :restaurants, dependent: :destroy
+	has_many :restaurants
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,8 @@
 class Restaurant < ApplicationRecord
 	mount_uploader :image, PhotoUploader
 	validates_presence_of :name
-	belongs_to :category
+
+	belongs_to :category, optional: true
+	
+	delegate :name, to: :category, prefix: true, allow_nil: true
 end
