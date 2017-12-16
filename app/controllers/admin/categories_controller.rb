1ada9c24a2918f400f76b6fabb6987e1ce93eeb0
eric07109 <eric07109@gmail.com>
2017-12-16 10:10:59 +0800

    minor front end changes to make the panel heading clean of page related functions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < Admin::BaseController
-	before_action :authenticate_admin
+
 	before_action :set_category, only: [:update, :destroy]
 
 	def index
--- note.md
+++
@@ -1,6 +0,0 @@
-#Notes on Ruby 
-* What is 'params' in Ruby?
-    - Why do we use find(params[:id]) syntax?
-
-#Todos
-* Make Restaurant and Category button separate tabs 
