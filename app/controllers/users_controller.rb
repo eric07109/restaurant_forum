f31b35276e3b66e48b86ab82be40c8668c1964fd
eric07109 <eric07109@gmail.com>
2017-12-28 21:31:52 +0800

    added edit user page, and you can only edit your own page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :update, :edit]
+
 	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if current_user != @user
+			redirect_to user_path(current_user)
+			flash[:alert] = "you cannot edit someone else profile"
+		else
+			@user.update(user_params)
+			redirect_to user_path(@user)
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :intro, :avatar)
+	end
+
+	def set_user
 		@user = User.find(params[:id])
 	end
+
 
 end
