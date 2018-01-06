d83440bc2fef26aaf23ccab054b2b3207490a2a9
eric07109 <eric07109@gmail.com>
2018-01-06 21:27:51 +0800

    add favourite/unfavourite action on RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-	before_action :set_user, only: [:show, :dashboard]
+	before_action :set_user, only: [:show, :dashboard, :favourite, :unfavourite]
 
 	def index
 		@restaurants = Restaurant.page(params[:page]).per(9)
@@ -18,10 +18,19 @@ class RestaurantsController < ApplicationController
 	def dashboard
 	end
 
+	def favourite
+		Favourite.create(restaurant: @restaurant, user: current_user)
+		redirect_back(fallback_location: root_path)
+	end
+
+	def unfavourite
+		Favourite.where(restaurant: @restaurant, user: current_user).destroy_all
+		redirect_back(fallback_location: root_path)
+	end
+
+
 	private
 	def set_user
 		@restaurant = Restaurant.find(params[:id])
 	end
-
-
 end
