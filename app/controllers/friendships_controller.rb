e69babd69f9cf18e6ef8b269a866ebf17bbfc9e2
eric07109 <eric07109@gmail.com>
2018-01-17 07:50:53 +0800

    created friend and unfriend buttons and corresponding controller actions

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,27 @@
 class FriendshipsController < ApplicationController
+	def create
+		@friendship = current_user.friendships.build(friend_id: params[:friend_id])
+
+		if @friendship.save
+			flash[:notice] = "Friendship added"
+			redirect_back(fallback_location: root_path)
+		else
+			flash[:alert] = @friendship.errors.full_messages.to_sentence
+			redirect_back(fallback_location: root_path)
+		end
+	end
+
+	def destroy
+		@friendship = current_user.friendships.where(friend_id: params[:id]).first
+
+		if @friendship.nil?
+			flash[:alert] = "Friendship cannot be deleted because it does not exist"
+			redirect_back(fallback_location: root_path)
+		else
+			@friendship.destroy
+			flash[:alert] = "Friendship destroyed"
+			redirect_back(fallback_location: root_path)
+		end
+
+	end
 end
